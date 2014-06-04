308a215ef8d58070ad9f63e736725942cc511969
Jacob Holman <jholman8@kim244-152294.c.byui.edu>
2014-06-04 13:21:51 -0600

    HAHA Animation Syntax Complete!!

--- swiftAnimation/ViewController.swift
+++ swiftAnimation/ViewController.swift
@@ -7,13 +7,94 @@
 //
 
 import UIKit
-
+import QuartzCore
 class ViewController: UIViewController {
-                            
+    
+    @IBOutlet var boxView : UIView
+    
+    
+    class Vehicle{
+        var numOfWheels = 0
+        var cost = 1000
+        var description : String{
+        return "\(numOfWheels) wheels"
+        }
+    }
+    class Car : Vehicle{
+        init() {
+          super.init()
+          numOfWheels = 4
+          cost = 3000
+                     }
+        override var description : String{
+        return "\(super.description) \(cost) dollars"
+        }
+        func addToCost(amount:Int){
+            cost += amount
+        }
+
+    }
+    class Truck : Car{
+        init() {
+            super.init()
+            cost = 5000
+        }
+        override var description : String{
+        return "Wow this truck cost \(cost) dollars"
+        }
+        var speed = 60
+    }
+    
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        //just messing around with classes
+        var car = Truck()
+        car.addToCost(222)
+        car.addToCost(-1)
+        println(car.description)
+        
+        car.speed = 69
+         println("\(car.speed) MPH")
+        car.addToCost(-500)
+        println("Would you take \(car.cost) for the truck")
+        println(countElements(car.description))
+        car.addToCost(400)
     }
+    //animations!!! took forever to guess this syntax!!!
+    
+   @IBAction func buttonTappedUp(AnyObject) {
+    UIView.animateWithDuration(2, animations: {() -> Void in
+        self.boxView.frame.offset(dx: 0, dy: -100)
+        self.boxView.backgroundColor = UIColor.blackColor()
+        })
+    }
+    @IBAction func buttonTappedDown(AnyObject) {
+        UIView.animateWithDuration(3, animations: {() -> Void in
+            self.boxView.frame.offset(dx: 0, dy: +100)
+            self.boxView.backgroundColor = UIColor.blueColor()
+            })
+    }
+    @IBAction func buttonTappedLeft(AnyObject) {
+        UIView.animateWithDuration(3, animations: {() -> Void in
+            self.boxView.frame.offset(dx: -100, dy: 0)
+            self.boxView.backgroundColor = UIColor.redColor()
+            })
+        
+    }
+    @IBAction func buttonTappedRight(AnyObject) {
+        
+        
+        UIView.animateWithDuration(3, animations: {() -> Void in
+           self.boxView.frame.offset(dx: +100, dy: 0)
+           self.boxView.backgroundColor = UIColor.cyanColor()
+        })
+    }
+
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
